bc28a891ef8390f4600a5039186f1c567660cb51
Gabe Juarez <empirenerd1138@gmail.com>
2021-08-23 16:01:16 -0700

    working on api Stuff

--- Basic-img-Search-API/src/components/app.js
+++ Basic-img-Search-API/src/components/app.js
@@ -1,19 +1,36 @@
 
 import React from 'react';
-import unsplash from '../api/unsplash';
+//import unsplash from '../api/unsplash';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
     state = { images: [] };
 
-    onSearchSubmit = async (term) => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: term },
-        });
+    // onSearchSubmit = async (term) => {
+    //     const response = await unsplash.get('/search/photos', {
+    //         params: { query: term },
+    //     });
 
-        this.setState({ images: response.data.results });
-    };
+    //     this.setState({ images: response.data.results });
+    // };
+
+    async onSearchSubmit(term) {
+        try {
+            const response = await axios.get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID'
+                }
+            })
+            console.log(response);
+            this.setState({ images: response.data.results });
+        } catch (error) {
+            console.log(error);
+        };
+
+    }
 
     render() {
         return (
